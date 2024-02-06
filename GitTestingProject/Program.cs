92ba4b84e7e9253cbe4fbbc5fa0c6ca75191ea66
Kaung Kaung <kaunghtetaung044@gmail.com>
2024-02-06 23:38:18 +0630

    Testing Branch

--- GitTestingProject/Program.cs
+++ GitTestingProject/Program.cs
@@ -2,4 +2,5 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("My name is nyi nyi aung");
 Console.WriteLine("This is Feature branch");
+Console.WriteLine("This is Testing branch");
 Console.WriteLine("hello testing");
